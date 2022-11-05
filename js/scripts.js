c4ef94c7facbe78413d2d6b701a01189a07c7b17
KayStation <matt0122@comcast.net>
2022-11-05 16:25:12 -0500

    Began working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
 let myLibrary = [];
 
-function Book(){
-    
+let totalBooks = 0;
+
+function Book(bookId){
+    this.bookId;
+    this.name;
+    this.author;
+    this.pageCount;
 }
 
 function addBookToLibrary(){
+    let bookId = new Book();
 
+    let name = new Book(prompt("Book name"));
+
+    let author = prompt("Author name");
+    let pageCount = prompt("Page count");
+
+    name.author = author;
+    name.pageCount = pageCount;
+
+    totalBooks++;
 }
