9edbac9f5f1225f24d46e928ed42beeb547d23e8
KayStation <matt0122@comcast.net>
2022-11-06 15:57:09 -0600

    Cleaned up.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,38 @@
+// DOM variables
 addButton = document.getElementById('addButton');
 
+// Library manipulation
 let myLibrary = [];
 
-let totalBooks = 0;
-
-function Book(name, author, pageCount){
+function Book(name, author, pageCount, read){
     this.name = name;
     this.author = author;
     this.pageCount = pageCount;
+    this.read = read;
+    // Function to delete
+
 }
 
 function addBookToLibrary(){
     let name = prompt("Book name");
     let author = prompt("Author");
     let pageCount = prompt("Number of pages");
+    
+    //* Add a toggle button for read.
 
     const newBook = new Book(name, author, pageCount);
 
     myLibrary.push(newBook);
-    totalBooks++;
+    
 }
 
+function removeBookFromLibrary(index){
+    myLibrary.splice(index, index);
+}
+
+
+
+// Buttons
 addButton.addEventListener('click', function(){
     addBookToLibrary();
 })
